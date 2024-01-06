0f860cf8e6f41dcf5f03408781faab30488928f5
KSK-SHOP <KSK-SHOP@DESKTOP-6AVD985>
2024-01-06 20:02:26 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictures = 52;
+            int row = 3;
+
+            int numberOfRows = pictures / row;
+            int beyondMeasure = pictures - (numberOfRows * row);
+
+            Console.WriteLine(numberOfRows);
+            Console.WriteLine(beyondMeasure);
+            Console.ReadKey();
+        }
+    }
+}
